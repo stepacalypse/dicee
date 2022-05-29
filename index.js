a22539d9f2702e04fc54c5ec8d5e00bff8b2894a
Stepan Aleksashin <62248937+stepacalypse@users.noreply.github.com>
2022-05-29 12:37:37 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-alert('Working!')
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
 var image1 = document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1);
